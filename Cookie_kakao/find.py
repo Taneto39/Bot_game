9a6c3a98061abcb0803366f00bd6688190050c39
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2025-03-06 16:37:46 +0700

    -change file to detected ep pic.

--- Cookie_kakao/find.py
+++ Cookie_kakao/find.py
@@ -5,11 +5,14 @@ import autopy
 
 def find():
     screen = autopy.bitmap.capture_screen()
-    a = autopy.bitmap.Bitmap.open('pic\\get_box.png')
-    print(screen.find_bitmap(a))
+    pics_name = ['heart.png', 'result.png', 'start1.png', 'start2.png']
+    ep = int(input("ep:"))
+    for pic_name in pics_name:
+        a = autopy.bitmap.Bitmap.open(f'pic\\{ep}\\{pic_name}')
+        print(screen.find_bitmap(a))
 
 
 if __name__ == '__main__':
     while True:
         find()
-        time.sleep(.1)
+        time.sleep(1)
