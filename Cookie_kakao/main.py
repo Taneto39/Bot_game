244092a95ceca035db6d763ada354df8f97a2ef2
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2025-03-15 18:31:33 +0700

    -fix .width(), .height() to .width, .height

--- Cookie_kakao/main.py
+++ Cookie_kakao/main.py
@@ -31,7 +31,7 @@ def click(pic_name, loop, sec, ep):
         pos = screen.find_bitmap(a)
         if pos:
             print(f'Found! click.')
-            autopy.mouse.move(pos[0] + random.randrange(a.width()), pos[1] + random.randrange(a.height()))
+            autopy.mouse.move(pos[0] + random.randrange(a.width), pos[1] + random.randrange(a.height))
             autopy.mouse.click()
             break
         print('Not found! Find again.')
@@ -49,7 +49,7 @@ def open_box(ep):
         pos = screen.find_bitmap(a)
         if pos:
             print(f'Found! click.')
-            autopy.mouse.move(pos[0] + random.randrange(a.width()), pos[1] + random.randrange(a.height()))
+            autopy.mouse.move(pos[0] + random.randrange(a.width), pos[1] + random.randrange(a.height))
             autopy.mouse.click()
             break
         print('Not found! Find again.')
@@ -185,7 +185,7 @@ def reset_game(ep):
         pos = screen.find_bitmap(app_icon)
         if pos:
             print('Found! click.')
-            autopy.mouse.move(pos[0] + random.randrange(app_icon.width()), pos[1] + random.randrange(app_icon.height()))
+            autopy.mouse.move(pos[0] + random.randrange(app_icon.width), pos[1] + random.randrange(app_icon.height))
             autopy.mouse.click()
             time.sleep(1)
         else:
